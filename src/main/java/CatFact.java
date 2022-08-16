0ee28f5604c555338af630787dc7e417872521d9
gigascript <gigascript@yandex.ru>
2022-08-16 22:23:34 +0700

    не много изменил toString

--- src/main/java/CatFact.java
+++ src/main/java/CatFact.java
@@ -43,7 +43,7 @@ public class CatFact {
 
     @Override
     public String toString() {
-        return "Post" +
+        return "CatFacts" +
                 "\n id=" + id +
                 "\n text=" + text +
                 "\n type=" + type +
